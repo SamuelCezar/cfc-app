23010ecce4e6035b99a112052d92a626a0e12ad2
Samuel Chagas <samuelcezar@outlook.com>
2022-06-15 14:35:05 -0300

    Inittial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Cálculo do IMC</h1>
+  <h2>Digite seu peso e altura para calcular seu IMC</h2>
+
+  <div class="div-imc">
+    <span class="p-float-label">
+      <InputText id="input-weight" type="text" v-model="weight" />
+      <label for="input-weight">Peso</label>
+    </span>
+  </div>
+
+  <div class="div-imc">
+    <span class="p-float-label">
+      <InputText id="input-height" type="text" v-model="height" />
+      <label for="input-height">Altura</label>
+    </span>
+  </div>
+
+  <Button style="margin-top: 1rem" label="Calcular" @click="calculate" />
+  <Button label="Limpar" @click="clear" />
+
+  <p class="label-result">Seu IMC é: {{ imc }}</p>
+  <p class="label-classification">
+    A classificação do seu IMC é: {{ classification }}
+  </p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      height: null,
+      weight: null,
+      imc: null,
+      classification: "",
+    };
+  },
+  methods: {
+    calculate: function () {
+      this.imc = (this.weight / (this.height * this.height)).toFixed(2);
+      if (this.imc < 18.5) {
+        this.classification = "Magreza";
+      } else if (this.imc >= 18.5 && this.imc < 25) {
+        this.classification = "Normal";
+      } else if (this.imc >= 25 && this.imc < 30) {
+        this.classification = "Sobrepeso";
+      } else if (this.imc >= 30 && this.imc < 40) {
+        this.classification = "Obesidade";
+      } else {
+        this.classification = "Obesidade mórbida";
+      }
+    },
+    clear: function () {
+      (this.height = ""), (this.weight = "");
+      this.imc = "";
+      this.classification = "";
+    },
+  },
+};
 </script>
 
 <style>
@@ -19,8 +64,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.div-imc {
+  margin-top: 1.5rem;
+}
+.label-result {
+  font-size: 2rem;
+}
+.label-classification {
+  font-size: 1.5rem;
 }
 </style>
